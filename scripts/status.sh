51ec3b814f8359b9775134c3ac004333ca52d72e
Luke Swithenbank <lswith@users.noreply.github.com>
2018-06-07 14:33:48 +1000

    Update status.sh

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
+set -eou pipefail
+
 app=$1
 namespace=${2:-default}
+
+echo "Checking app and namesapce are set"
 
 if [ -z "$app" ] || [ -z "$namespace" ]; then
     echo -e "\nPlease specify the App name and the namespace (default if empty)!\n";
     exit 1;
 else
     # Get the timeout from the deployment and divide into 10 seconds spaced chunks
-    declare -i x=$(( `kubectl get deployment $app -o=jsonpath='{.spec.progressDeadlineSeconds}' --namespace=$namespace`/10 ))
+    echo "Getting ProgressDeadlineSeconds"
+    set +e
+    declare -i progressSeconds
+    progressSeconds=$( { kubectl get deployment $app -o=jsonpath='{.spec.progressDeadlineSeconds}' --namespace=$namespace })
+    set -e
+    
+    if [ $? != 0 ]; then
+        echo "Couldn't get progress seconds";
+        exit 1;
+    fi
+    
+    declare -i x
+    x=$(( $progressSeconds/10 ))
+    
     echo "Setting timeout: $x runs"
     i=0
     while [ $i -le $x ]
     do
+        echo "checking rollout status"
         status=`kubectl rollout status deploy/$app --namespace=$namespace --watch=false 2>&1`
+        
         if [ $i -eq $x ]; then
             echo $status;
             echo "Exiting due to timeout. The deployment for $app has probably failed!"
